d268e721e3fad9e460ae96d9f3e80a644a2f92f8
glenvollmer <glenvollmer@glenvollmers-MacBook-Pro.local>
2022-03-07 14:22:54 -0600

    cleaned up code and dependencies

--- src/app.py
+++ src/app.py
@@ -6,11 +6,11 @@ import sys
 from urllib import request
 from bs4 import BeautifulSoup
 
-''' HOW TO USE:$ python3 app.py DATPIFF_URL_GOES_HERE '''
 
-def main(url):
+def get_mixtape_data(url):
+
+	mixtape_data = {}
 	base_url = 'http://www.datpiff.com'
-	save_directory = '../downloads'
 
 	mixtap_page_data = request.urlopen(url)
 	mixtape_page = BeautifulSoup(mixtap_page_data, 'html.parser')
@@ -19,10 +19,12 @@ def main(url):
 		attrs = {
 			'onclick': re.compile('openMixtape')
 		})
-
+	
+	html_token_div_str = str(html_token_div)
+	
 	token = re.search(
 		r'\((.*?)\)', 
-		str(html_token_div)
+		html_token_div_str
 	).group(1).split('\'')[1]
 
 	player_data = request.urlopen(base_url + '/player/' + token)
@@ -39,14 +41,38 @@ def main(url):
 	script = iframe_page.select(
 		'script[src="/js/player.js"]:first-of-type + script'
 		)[0].getText()
+	
+	mixtape_data['url_track_prefix'] = re.findall(
+		r'\s*var trackPrefix = \'(.*?)\';', 
+		script)
 
-	url_track_prefix = re.findall(r'\s*var trackPrefix = \'(.*?)\';', script)
-	track_objs = re.findall(r'\s*playerData\.tracks\.push\((.*?)\);', script)
-	mixtape_title = re.findall(r'\s*\"title\":\"(.*?)\"', script)[0].replace('\'', '')
-	artist_name = re.findall(r'\s*\"artist\":\"(.*?)\"', script)[0].replace('\'', '')
+	mixtape_data['track_data']= re.findall(
+		r'\s*playerData\.tracks\.push\((.*?)\);', 
+		script)
 
-	for t in track_objs:
-		url_track = re.findall(r'\s*concat\((.*?)\),', t)[0].strip().replace('\'', '')
+	mixtape_data['mixtape_title'] = re.findall(
+		r'\s*\"title\":\"(.*?)\"', 
+		script)[0].replace('\'', '')
+
+	mixtape_data['artist_name'] = re.findall(
+		r'\s*\"artist\":\"(.*?)\"', 
+		script)[0].replace('\'', '')
+
+	return mixtape_data
+
+
+def download_mixtape(save_dir, mixtape_data):
+	
+	save_directory = save_dir
+	track_data = mixtape_data['track_data']
+	url_track_prefix = mixtape_data['url_track_prefix']
+	artist_name = mixtape_data['artist_name']
+	mixtape_title = mixtape_data['mixtape_title']
+
+	for t in track_data:
+		url_track = re.findall(
+			r'\s*concat\((.*?)\),', 
+			t)[0].strip().replace('\'', '')
 		
 		audio_data = request.urlopen(
 			(url_track_prefix[0] + url_track).replace(' ', '%20'))
@@ -63,7 +89,20 @@ def main(url):
 			f.flush()
 			os.fsync(f.fileno())
 			f.close()
+	return
+
+
+def get_cli_input():
+	cli_input_data = {}
+	
+	cli_input_data['url'] = str(sys.argv[1])
+	cli_input_data['save_dir'] = str(sys.argv[2])
+
+	return cli_input_data
+
 
 if __name__ == '__main__':
-	url = str(sys.argv[1])
-	main(url)
+	cli_input = get_cli_input()
+
+	mixtape_data = get_mixtape_data(cli_input['url'])
+	download_mixtape(cli_input['save_dir'], mixtape_data)
